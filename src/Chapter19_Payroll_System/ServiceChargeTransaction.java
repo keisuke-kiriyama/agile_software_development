7a270749fc041549359c5d2c3199bb947877ec64
kiriyama-keisuke <kiriyama_keisuke@r.recruit.co.jp>
2020-11-10 23:48:03 +0900

    List 19.2

--- src/Chapter19_Payroll_System/ServiceChargeTransaction.java
+++ src/Chapter19_Payroll_System/ServiceChargeTransaction.java
@@ -1,5 +1,25 @@
 package Chapter19_Payroll_System;
 
-public class ServiceChargeTransaction {
-    `````
+import java.awt.geom.AffineTransform;
+
+public class ServiceChargeTransaction implements Transaction {
+    private int itsMemberId;
+    private long itsDate;
+    private double itsAmount;
+
+    public ServiceChargeTransaction(int memberId, long date, double amount) {
+        itsMemberId = memberId;
+        itsDate = date;
+        itsAmount = amount;
+    }
+
+    @Override
+    public void Execute() {
+        Employee e = PayrollDatabase.GetUnionMember(itsMemberId);
+        Affiliation af = e.GetAffiliation();
+        if (af instanceof UnionAffiliation) {
+            UnionAffiliation uaf = (UnionAffilication) af;
+            uaf.AddServiceCharge(itsDate, itsAmount);
+        }
+    }
 }
